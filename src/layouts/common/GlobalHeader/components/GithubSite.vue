8701ccc1f7914485e7fee1bae0db2366ebf97fd6
殊怀丶 <lvzhihe_123@qq.com>
2022-05-25 15:27:15 +0800

    feat: 在首页添加logo

--- src/layouts/common/GlobalHeader/components/GithubSite.vue
+++ src/layouts/common/GlobalHeader/components/GithubSite.vue
@@ -1,11 +1,13 @@
 <template>
     <hover-container class="w-40px h-full" tooltip-content="github">
-        <a class="flex-center" href="https://github.com/OwiviOsa/backend" target="_blank">
-            <icon-uil:github-alt class="text-20px text-[#666]"/>
+        <a class="flex-center" href="https://github.com/Vehicle-Repairer" target="_blank">
+            <Icon icon="uil:github-alt" class="text-20px text-[#666]"/>
         </a>
     </hover-container>
 </template>
 
 <script lang="ts" setup>
-import {HoverContainer} from '@/components';</script>
-<style scoped></style>
+import {HoverContainer} from '@/components';
+import {Icon} from '@iconify/vue';</script>
+<style scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,9 @@
 <template>
-
+    <div class="text-8xl font-bold italic text-primary transition duration-300 ease-in-out text-center h-full flex">
+        <div class="underline decoration-teal-500 decoration-4 mx-auto mt-1/8">Vehicle Repairer</div>
+    </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, Ref, ref } from 'vue';
-import { FormInst, FormItemRule, useMessage } from 'naive-ui';
-import { addRepairItem, getMyInfo, modifyInfo, getRepairItems, finishAssignment } from '@/apis';
-import { mode } from 'crypto-js';
-
-
+<script lang="ts" setup>
 </script>
 <style scoped>
 </style>
-
-
